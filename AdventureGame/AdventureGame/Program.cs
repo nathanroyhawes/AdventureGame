320d25e56149fc49339ceb960f968605c0d4ff1d
nathanroyhawes <nathanhawes@proton.me>
2023-03-14 19:41:29 -0400

    adding VS files

---
+++ AdventureGame/AdventureGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventureGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+        }
+    }
+}
